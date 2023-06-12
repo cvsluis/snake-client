abb5eb73bcb47a69269b138e49e7a791f58caa50
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-12 15:59:00 -0700

    Implement move commands

--- input.js
+++ input.js
@@ -1,5 +1,8 @@
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function() {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,10 +11,18 @@ const setupInput = function() {
   return stdin;
 };
 
-const handleUserInput = function(key) {
+const handleUserInput = (key) => {
   // exit on ctrl+c
   if (key === '\u0003') {
     process.exit();
+  } else if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === 's') {
+    connection.write("Move: down");
+  } else if (key === 'd') {
+    connection.write("Move: right");
   }
 };
 
