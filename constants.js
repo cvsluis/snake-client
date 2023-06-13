91c1b1bc8147b27ef5860873f0ae9b571feb0130
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-12 19:49:15 -0700

    Add input key message

--- constants.js
+++ constants.js
@@ -1,16 +1,17 @@
 const IP = 'localhost';
 const PORT = 50541;
 
-const playerName = "Cat";
+const playerName = "CAT";
 
 const inputKeys = {
   w: "Move: up",
   a: "Move: left",
   s: "Move: down",
   d: "Move: right",
-  h: "Say: Snek is hungry",
+  h: "Say: Snake is hungry",
   j: "Say: I will win!",
-  k: "Say: Gotcha"
+  k: "Say: Gotcha",
+  l: "Say: nom nom nom"
 };
 
 module.exports = {
